ad97851fa718ad2da1f1224fca3dfe1cf5c572c6
einar <aglen>
2021-07-06 12:22:59 +0200

    Fixes

--- routes/image.js
+++ routes/image.js
@@ -27,14 +27,22 @@ cloudinary.config({
     api_secret: process.env.API_SECRET,
 });
 
+/*router.post("/update", async (request, response) => {
+    try {
+        let res = await cloudinary.api.update("uf244gwnizoh97vg07fr", {
+            filename: "test"
+        });
+        response.send({ status: true, res: res });
+    } catch (error) {
+        response.send({ status: false, message: error });
+    }
+});*/
+
 router.get("/all", authenticateToken, async (request, response) => {
     try {
         let res = await cloudinary.search
-            .expression(
-                "resource_type:image"
-            )
+            .expression("resource_type:image")
             .sort_by("public_id", "desc")
-            .max_results(30)
             .execute()
             .then((result) => result);
         response.send({ status: true, images: res.resources });
@@ -43,31 +51,38 @@ router.get("/all", authenticateToken, async (request, response) => {
     }
 });
 
-router.post("/upload", authenticateToken, fileUpload.single("image"), (request, response, next) => {
-    const streamUpload = (request) => {
-        return new Promise((resolve, reject) => {
-            let stream = cloudinary.uploader.upload_stream((error, result) => {
-                if (result) {
-                    resolve(result);
-                } else {
-                    reject(error);
-                }
+router.post(
+    "/upload",
+    authenticateToken,
+    fileUpload.single("image"),
+    (request, response, next) => {
+        const streamUpload = (request) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+                streamifier.createReadStream(request.file.buffer).pipe(stream);
             });
-            streamifier.createReadStream(request.file.buffer).pipe(stream);
-        });
-    };
+        };
 
-    const upload = async (req) => {
-        try {
-            let result = await streamUpload(request);
-            response.send({ status: true, result: result });
-        } catch (error) {
-            response.send({ status: false, message: error.toString() });
-        }
-    };
+        const upload = async (req) => {
+            try {
+                let result = await streamUpload(request);
+                response.send({ status: true, result: result });
+            } catch (error) {
+                response.send({ status: false, message: error.toString() });
+            }
+        };
 
-    //execute
-    upload(request);
-});
+        //execute
+        upload(request);
+    }
+);
 
 module.exports = router;
